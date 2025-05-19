19fbd278725b8ba2dedfa8811ec2f2d04dcd38f7
Sonu <sonuroy2029@gmail.com>
2025-05-19 21:18:09 +0530

    feat: render products on the checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,12 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: 4
+  }, {
+    productId: '82bb68d7-ebc9-476a-989c-c78a40ee5cd9',
+    quantity: 2
+  }
+];
 
 export function addToCart(productId, quantity) {
   let matchingItem;
