ab5a63b2a555ad2e1bd1b3312e2d55c8a62f58b6
Sonu <sonuroy2029@gmail.com>
2025-05-19 21:24:02 +0530

    fix: resolve issue with radio selectors not working properly

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -49,7 +49,7 @@ cart.forEach((cartItem) => {
           <div class="delivery-option">
             <input type="radio" checked
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingItem.id}">
             <div>
               <div class="delivery-option-date">
                 Tuesday, June 21
@@ -62,7 +62,7 @@ cart.forEach((cartItem) => {
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingItem.id}">
             <div>
               <div class="delivery-option-date">
                 Wednesday, June 15
@@ -75,7 +75,7 @@ cart.forEach((cartItem) => {
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingItem.id}">
             <div>
               <div class="delivery-option-date">
                 Monday, June 13
