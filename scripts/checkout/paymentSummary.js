8d1aac012e3081db73694a5172058499bec64a6c
Sonu <sonuroy2029@gmail.com>
2025-06-10 13:58:03 +0530

    feat: render payment summary items based on cart quantity

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -6,6 +6,7 @@ import {formatCurrency} from '../utils/money.js'
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let productQuantity = 0;
 
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
@@ -13,6 +14,8 @@ export function renderPaymentSummary() {
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
+
+    productQuantity += cartItem.quantity;
   });
 
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
@@ -25,7 +28,7 @@ export function renderPaymentSummary() {
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items (${productQuantity}):</div>
       <div class="payment-summary-money">
         $${formatCurrency(productPriceCents)}
       </div>
