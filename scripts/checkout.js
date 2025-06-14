9870ea9135c6c61bc65dfd743ed7a4acce6aef24
Sonu <sonuroy2029@gmail.com>
2025-06-14 15:12:23 +0530

    refactor: replace XMLHttpRequest with fetch in cart.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,16 +2,12 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {loadProductsFetch} from '../data/products.js'
-import {loadCart} from '../data/cart.js';
+import {loadCartFetch} from '../data/cart.js';
 
 async function loadPage() {
   await loadProductsFetch();
 
-  const value = new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  });
+  await loadCartFetch();
 
   renderOrderSummary();
   renderPaymentSummary();
